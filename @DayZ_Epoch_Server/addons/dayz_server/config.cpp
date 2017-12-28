7f6cc46a0983ae5654dc3c564716f9082e4faa44
TheFirstNoob <TheAssassin666@yandex.ru>
2017-12-28 12:49:28 +0300

    Добавлены чистые файлы DayZ Epoch 1.0.6.2

---
+++ @DayZ_Epoch_Server/addons/dayz_server/config.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: Produced from mikero's Dos Tools Dll version 6.08
+//'now' is Sun Mar 12 16:41:24 2017 : 'file' last modified on Sun Mar 12 16:25:15 2017
+//http://dev-heaven.net/projects/list_files/mikero-pbodll
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+//Class dayz_server : config.bin{
+class CfgPatches
+{
+	class dayz_server
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"dayz_code"};
+	};
+};
+//};
